0381c32b291f8c906d872474e1fc13baf5f7d6ce
Katherina <fedoseeva2292@gmail.com>
2024-03-11 17:06:07 +0100

    Book page
    - styles
    - responsive

--- src/pages/Book/Book.tsx
+++ src/pages/Book/Book.tsx
@@ -1,32 +1,49 @@
 import { useParams } from 'react-router-dom';
 import { useGetBookByIdQuery } from '../../services/bookService';
 import ErrorHandler from '../../components/ErrorHandler/ErrorHandler';
+import spinner from '../../assets/spinner.gif';
+import './Book.scss';
 
 function Book() {
   const { id } = useParams();
-  const { data, error } = useGetBookByIdQuery(id as string);
+  const { data, isFetching, isLoading, error } = useGetBookByIdQuery(
+    id as string
+  );
+
+  console.log(isFetching);
 
   return (
     <section className="book_page_container">
       <div className="book_page_left">
-        <img
-          src={data?.volumeInfo.imageLinks.smallThumbnail}
-          alt="book_cover"
-        />
+        {isLoading && isFetching ? (
+          spinner
+        ) : (
+          <img
+            src={data?.volumeInfo.imageLinks.smallThumbnail}
+            alt="book_cover"
+          />
+        )}
       </div>
       <div className="book_page_right">
+        <h2 className="title">{data?.volumeInfo.title}</h2>
         <div className="category">
-          {data?.volumeInfo.categories?.map((cat) => cat)}
+          <span>{`Category: `}</span>
+          {data?.volumeInfo.categories?.map((cat) => cat) || 'no information'}
         </div>
         <div className="page_count">
-          Page count: {data?.volumeInfo.pageCount}
+          <span>{`Page count: `}</span>
+          {data?.volumeInfo.pageCount || 'no information'}
         </div>
-        <h2 className="title">{data?.volumeInfo.title}</h2>
         <div className="author">
-          Author: {data?.volumeInfo.authors?.map((author) => author)}
+          <span>{`Author: `}</span>
+          {data?.volumeInfo.authors?.map((author) => author) ||
+            'no information'}
         </div>
-        <div className="description">{data?.volumeInfo.description}</div>
-        div.
+        <div className="description">
+          <span>{`Description: `}</span>
+          {data?.volumeInfo.description?.replace(/<[^>]*>/g, '') ||
+            'no information'}
+        </div>
       </div>
       <ErrorHandler error={error} />
     </section>
